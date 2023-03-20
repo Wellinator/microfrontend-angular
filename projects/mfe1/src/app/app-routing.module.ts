ba631840b8fb6c0ed64621b33db1a9446edf5b04
Wellinator <wellcoj@gmail.com>
2023-03-20 17:31:02 -0300

    feat: adds my-component route

--- projects/mfe1/src/app/app-routing.module.ts
+++ projects/mfe1/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'my-component',
+    loadChildren: () =>
+      import('./components/component-a/component-a.module').then(
+        (m) => m.ComponentAModule
+      ),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
