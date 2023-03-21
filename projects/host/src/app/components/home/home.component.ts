76cd5569c7f417ac1a345fab9956505b33782ada
Wellinator <wellcoj@gmail.com>
2023-03-21 17:40:34 -0300

    feat: update home component

--- projects/host/src/app/components/home/home.component.ts
+++ projects/host/src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  title = 'home';
+  user$ = this.auth.user$;
+
+  constructor(public auth: AuthService) {}
 }
